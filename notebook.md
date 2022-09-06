d163a9d81779004a480093fe8fdeb303e75d8bf0
Luke <1263810658@qq.com>
2022-09-06 16:23:31 +0300

    update

--- notebook.md
+++ notebook.md
@@ -16,11 +16,29 @@ project，  4-5个学生。  10月20日proposal，  11月26日 report， 12月4-
 
 R是大小写敏感的。
 
-
+考试会考一些函数. 
 
 
 
 问题
 
 1. 为啥明明有东西， ls（）显示没有东西。
+
+
+
+
+
+ts : time series
+
+data frame: 一张表, 允许数据有不同的mode
+
+Subset, 取一个子集.
+
+apply :   在数据上apply函数,   用不同维度来展示数据.
+
+sweep  : 也是apply函数, 比如求和mean的距离. element wise计算.
+
+within, 新增一列, 可以通过别的列计算出来.
+
+
 
--- problemlist/scriptV2.R
+++ problemlist/scriptV2.R
@@ -1,4 +1,4 @@
-
+## --------------------------
 getwd()
 
 ls()
@@ -6,28 +6,12 @@ ls()
 sd
 
 data()
-
 data(cars)
-
 plot(cars)
-
 head(cars)
-
 str(cars)
-
-2+3; 5*4
-
 var
-
-ls()
-
 rm(list=ls())
-
-a <- 12
-
-ls()
-
-
 help(log)
 
 ?lm
@@ -35,3 +19,46 @@ help(log)
 ?'*'
 
 ??truehist
+
+x <- c(14:16,12,11,13,17)
+
+order(x)
+
+x[order(x)]
+
+dim(x)
+
+apply(iris3, 2, mean) # 用不同维度显示数据.
+
+## --------------------------
+# problem list 1 
+rep(10:1,c(5:1,1:5)) # 每个的个数不同
+
+seq(100,102,length.out = 10)
+
+1:5 + rep(0:4 , each = 5)
+
+# exercise 2 
+# 就是落在圆内的点数量.疏通机
+
+x <- runif(10000,-1,1)
+y <- runif(10000,-1,1)
+z <- x^2 + y ^2
+asum <- sum(as.numeric(z<1))
+(piest4 <- 4*asum/10000)
+
+(error4 = abs(pi-piest4) )
+
+
+str(mtcars)
+
+
+# exercise 4
+
+pp = c(0.1,0.2)
+sample(samp1,100,replace =T, prob = pp)
+
+
+table(fact1)
+
+
